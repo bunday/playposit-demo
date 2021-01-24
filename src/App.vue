2368dd6df3d1578e50924da90167826cb11e91ce
Zadat Olayinka <bundayy@outlook.com>
2021-01-24 22:23:51 +0100

    fronnt end clean up set up

--- src/App.vue
+++ src/App.vue
@@ -6,34 +6,18 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h3>Interactive 360 Video Demo</h3>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://zadatolayinka.dev/"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">by Zadat Olayinka</span>
+        <v-icon>mdi-person</v-icon>
       </v-btn>
     </v-app-bar>
 
