716f151f14a2a56daa281019d03847f98f2932e2
Zadat Olayinka <bundayy@outlook.com>
2021-01-27 19:26:37 +0100

    add link and iframe element to view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row class="text-center">
       <v-col class="mb-4">
-        <p class="subheading font-weight-regular">Instruction will go here</p>
+        <p class="subheading font-weight-regular">Please click on the setup button twice to start this Demo</p>
       </v-col>
       <v-col class="mb-5" cols="12">
         <video
@@ -46,7 +46,7 @@
       <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold mb-3">Camera Status</h2>
 
-        <v-form v-model="valid">
+        <v-form>
           <v-row>
             <v-col cols="12" md="4">
               <v-text-field
@@ -132,7 +132,7 @@ export default {
         this.player.vr().renderer.domElement
       );
       controls.addEventListener("change", this.updateCameraPosition);
-      this.addElement()
+      this.addElement();
     },
     getPlayer() {
       // get the camera status and update the camera data
@@ -160,32 +160,53 @@ export default {
     },
     addElement() {
       let { scene, camera } = this.player.vr();
-      var element = document.createElement("div");
+      const link = this.addInformationLink();
+      scene.add(link.object);
+      camera.position.z = 5;
+      link.labelRenderer.render(scene, camera);
+    },
+    addInformationLink() {
+      var element = document.createElement("a");
       element.className = "hotspot";
-      element.innerHTML = "Testing stuff";
+      element.innerHTML = "Information about the park";
+      element.target = '_blank';
+      element.href = 'https://en.wikipedia.org/wiki/Parks_and_open_spaces_in_London';
       var object = new CSS2DObject(element);
-      object.element.onclick = function () {
-        object.element.style.backgroundColor = "#f1060e";
-      };
 
       object.position.x = 0;
       object.position.y = 0;
       object.position.z = 7;
 
-      // object.element.onclick = function () {
-      //   this.parent.position.y += 10;
-      // };
       let labelRenderer = new CSS2DRenderer();
-      labelRenderer.setSize(40, 40);
+      labelRenderer.setSize(400, 400);
       labelRenderer.domElement.style.position = "absolute";
-      // labelRenderer.domElement.style.top = "280px";
-      labelRenderer.domElement.className = "hotspot";
+      labelRenderer.domElement.style.top = "300px";
+      labelRenderer.domElement.style.left = "780px";
       document.body.appendChild(labelRenderer.domElement);
-
-      scene.add(object);
-      camera.position.z = 5;
-      labelRenderer.render(scene, camera);
+      return { object, labelRenderer}
     },
+    addIframeVideo() {
+      var element = document.createElement("iframe");
+      element.className = "hotspot-iframe";
+      element.height="200" 
+      element.width="300" 
+      element.title="Iframe Duck video"
+      
+      element.src = 'https://www.youtube.com/embed/KESOibZQlhA';
+      var object = new CSS2DObject(element);
+
+      object.position.x = 0;
+      object.position.y = 0;
+      object.position.z = 7;
+
+      let labelRenderer = new CSS2DRenderer();
+      labelRenderer.setSize(400, 400);
+      labelRenderer.domElement.style.position = "absolute";
+      labelRenderer.domElement.style.top = "100px";
+      labelRenderer.domElement.style.left = "480px";
+      document.body.appendChild(labelRenderer.domElement);
+      return { object, labelRenderer}
+    }
   },
   data: () => ({
     player: null,
@@ -196,11 +217,12 @@ export default {
 <style>
 @import url("https://vjs.zencdn.net/7.2.3/video-js.css");
 .hotspot {
-  width: 50px;
+  width: 200px;
   background: #e4bdbe;
   border-radius: 10%;
   padding: 5px;
   cursor: pointer;
+  text-decoration: none;
 }
 .hotspot:hover {
   background: #e44db1;
